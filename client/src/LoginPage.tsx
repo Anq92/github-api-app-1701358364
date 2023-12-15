57df4ba0833d76e9f244f68a46b84b25f4519487
Anq92 <anna.wilczura@gmail.com>
2023-12-15 18:21:30 +0100

    chore: move app source files to client folder

--- client/src/LoginPage.tsx
+++ client/src/LoginPage.tsx
@@ -1,8 +1,14 @@
 import "./LoginPage.sass";
+import { useEffect } from "react";
 
 const client_id = "389060375848c3349fc3";
 
 function LoginPage() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams.get("code"));
+  }, []);
   function loginWithGithub() {
     window.location.assign(
       "https://github.com/login/oauth/authorize?client_id=" + client_id
