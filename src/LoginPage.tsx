f141c009200bf54d87beb49c4db46670cc57fd61
Anq92 <anna.wilczura@gmail.com>
2023-12-15 17:55:32 +0100

    feat: redirect to GitHub OAuth login page

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,9 +1,17 @@
 import "./LoginPage.sass";
 
+const client_id = "389060375848c3349fc3";
+
 function LoginPage() {
+  function loginWithGithub() {
+    window.location.assign(
+      "https://github.com/login/oauth/authorize?client_id=" + client_id
+    );
+  }
+
   return (
     <div className="login-page-container">
-      <button className="login-button">
+      <button className="login-button" onClick={loginWithGithub}>
         <span>Log in with GitHub</span>
         <img className="gh-logo" src="src/assets/github-mark-white.svg"></img>
       </button>
