7976a53aedab05c3b46537ffa9c69b2226c45a9b
Anq92 <anna.wilczura@gmail.com>
2024-01-11 20:06:07 +0100

    chore: normalize port value, refactor /n use config.json file for port number remove unused route

--- server.js
+++ server.js
@@ -1,55 +1,67 @@
 var express = require("express");
 var cors = require("cors");
+var dotenv = require("dotenv");
+
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 var bodyParser = require("body-parser");
 
-require("dotenv").config();
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
+dotenv.config();
+dotenv.config({ path: `.env`, override: true });
+
+const configPath = "./config.json";
+const config = require(configPath);
+
+const port = normalizePort(process.env.PORT || config.port || "4000");
+const envVersion = process.env.ENV_VERSION || "UNKNOWN";
+
+const clientId = process.env.CLIENT_ID || "UNKNOWN";
+const clientSecret = process.env.CLIENT_SECRET || "UNKNOWN";
 
 var app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
-app.get("/getAccessToken", async function (req, res) {
-  req.query.code;
-  const params =
+
+app.get("/", (req, res) => {
+  res.send(`Yes? v5 / ${envVersion}`);
+});
+
+app.get("/token", async function (req, res) {
+  const queryString =
     "?client_id=" +
-    client_id +
+    clientId +
     "&client_secret=" +
-    client_secret +
+    clientSecret +
     "&code=" +
     req.query.code;
 
-  await fetch("https://github.com/login/oauth/access_token" + params, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
-    });
+  var response = await fetch(
+    "https://github.com/login/oauth/access_token" + queryString,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  var data = await response.json();
+  res.json(data);
 });
 
-app.get("/getUserData", async function (req, res) {
-  req.get("Authorization");
-  await fetch("https://api.github.com/user", {
-    method: "GET",
-    headers: {
-      Authorization: req.get("Authorization"),
-    },
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      res.json(data);
-    });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
-app.listen(4000);
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
