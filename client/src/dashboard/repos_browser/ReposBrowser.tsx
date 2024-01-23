61f431080389f576fbe07f430dceed2dff3c56ee
Anq92 <anna.wilczura@gmail.com>
2024-01-23 23:05:48 +0100

    feat: display selected repo info

--- client/src/dashboard/repos_browser/ReposBrowser.tsx
+++ client/src/dashboard/repos_browser/ReposBrowser.tsx
@@ -1,11 +1,17 @@
-import { AutoComplete } from "primereact/autocomplete";
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import './ReposBrowser.sass';
+import { AutoComplete, AutoCompleteChangeEvent } from "primereact/autocomplete";
 import { RepoData } from "../../types";
 import { useState } from "react";
 
 function ReposBrowser({ ...props }) {
 
     const reposData = props.reposData;
-    const [selectedRepo, setSelectedRepo] = useState<RepoData | null>(null)
+
+    const [selectedRepo, setSelectedRepo] = useState<RepoData | null>(null);
+    const [displayedRepo, setDisplayedRepo] = useState<RepoData | null>(null);
     const [filteredRepos, setFilteredRepos] = useState<RepoData[]>([]);
 
     const searchRepo = (event: { query: string }) => {
@@ -24,16 +30,30 @@ function ReposBrowser({ ...props }) {
         }, 250);
     }
 
-    const repoList = reposData.map((repo: RepoData) => {
-        return <li>{repo.name}</li>
-    })
+    const handleOnChange = (e: AutoCompleteChangeEvent) => {
+        setSelectedRepo(e.value);
+    }
+
     return (
-        <>
+        <div className='browser-container'>
             <h3>Search for repositories...</h3>
-            <AutoComplete value={selectedRepo} suggestions={filteredRepos} completeMethod={searchRepo} field="name" onChange={(e) => setSelectedRepo(e.value)} />
-            <ul>{repoList}</ul>
-        </>
-    )
+            <AutoComplete
+                value={selectedRepo}
+                suggestions={filteredRepos}
+                completeMethod={searchRepo}
+                field="name"
+                onChange={handleOnChange}
+                onClear={() => setDisplayedRepo(null)}
+                onSelect={(e) => setDisplayedRepo(e.value)}
+                placeholder="repository name"
+            />
+            {displayedRepo &&
+                <div className='search-result'>
+                    <span><b>Name:</b> {displayedRepo.name}</span>
+                    <span><b>Stars:</b> {displayedRepo.stargazers_count}</span>
+                </div>}
+        </div>
+    );
 }
 
-export default ReposBrowser
+export default ReposBrowser;
