927f5dbeb751c564bfacf565bc2c70d2c8fe7daf
Anq92 <anna.wilczura@gmail.com>
2024-01-25 23:08:20 +0100

    feat: handle no matches

--- client/src/dashboard/repos_browser/ReposBrowser.tsx
+++ client/src/dashboard/repos_browser/ReposBrowser.tsx
@@ -2,9 +2,9 @@ import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.css';
 import './ReposBrowser.sass';
-import { AutoComplete, AutoCompleteChangeEvent } from "primereact/autocomplete";
+import { AutoComplete, AutoCompleteChangeEvent, AutoCompleteSelectEvent } from "primereact/autocomplete";
 import { RepoData } from "../../types";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function ReposBrowser({ ...props }) {
 
@@ -12,7 +12,11 @@ function ReposBrowser({ ...props }) {
 
     const [selectedRepo, setSelectedRepo] = useState<RepoData | null>(null);
     const [displayedRepo, setDisplayedRepo] = useState<RepoData | null>(null);
-    const [filteredRepos, setFilteredRepos] = useState<RepoData[]>([]);
+    const [filteredRepos, setFilteredRepos] = useState<RepoData[] | null>(null);
+    const [isShown, setIsShown] = useState(false);
+
+    const matches = useRef<boolean | null>(null);
+    const autoCompleteRef = useRef<AutoComplete>(null);
 
     const searchRepo = (event: { query: string }) => {
         setTimeout(() => {
@@ -34,17 +38,68 @@ function ReposBrowser({ ...props }) {
         setSelectedRepo(e.value);
     }
 
+    const handleOnClear = () => {
+        setDisplayedRepo(null);
+    }
+
+    const handleOnSelect = (e: AutoCompleteSelectEvent) => {
+        if (e.value !== noMatchesObj) {
+            setSelectedRepo(e.value);
+            setDisplayedRepo(e.value);
+        } else {
+            setSelectedRepo(null);
+        }
+    }
+
+    const handleOnShow = () => {
+        setIsShown(true);
+    }
+
+    const handleOnHide = () => {
+        setIsShown(false);
+    }
+
+    const noMatchesObj = {
+        name: "no matches found..."
+    }
+
+    useEffect(() => {
+        if (isShown) {
+            matches.current = !!filteredRepos!.length;
+
+            const preventHighlight = (e: KeyboardEvent) => {
+                if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+                    e.stopImmediatePropagation();
+                }
+            }
+
+            if (matches.current === false) {
+                const overlay = autoCompleteRef!.current!.getOverlay();
+                overlay.style.pointerEvents = "none";
+                window.addEventListener("keydown", preventHighlight, true);
+            }
+
+            return () => { window.removeEventListener("keydown", preventHighlight, true) };
+        }
+
+    }, [isShown, filteredRepos])
+
+
     return (
         <div className='browser-container'>
             <h3>Search for repositories...</h3>
             <AutoComplete
+                ref={autoCompleteRef}
+                forceSelection
                 value={selectedRepo}
-                suggestions={filteredRepos}
+                suggestions={filteredRepos?.length ? filteredRepos : [noMatchesObj]}
                 completeMethod={searchRepo}
                 field="name"
                 onChange={handleOnChange}
-                onClear={() => setDisplayedRepo(null)}
-                onSelect={(e) => setDisplayedRepo(e.value)}
+                onClear={handleOnClear}
+                onHide={handleOnHide}
+                onSelect={handleOnSelect}
+                onShow={handleOnShow}
                 placeholder="repository name"
             />
             {displayedRepo &&
